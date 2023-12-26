57f5ed1a7ffc18c3cceed7d88440ff56b7d40c10
Joy_Boy <mihirkhetalpar111@gmail.com>
2023-12-26 18:29:26 +0530

    new folder add and all manga books img update

--- BooksHub/JS/Common.js
+++ BooksHub/JS/Common.js
@@ -1,7 +1,18 @@
-                                                    // PRELOADER 
-document.addEventListener("DOMContentLoaded", function() {
-let loader = document.getElementById("preloader");
-setTimeout(function() {
-loader.style.display = "none";
-   }, 2000); 
+// PRELOADER
+document.addEventListener("DOMContentLoaded", function () {
+  let loader = document.getElementById("preloader");
+  setTimeout(function () {
+    loader.style.display = "none";
+  }, 2000);
+});
+
+// Detect scroll events and update the width of the scroll indicator
+window.addEventListener("scroll", function () {
+  const scrollIndicator = document.querySelector(".scroll-indicator");
+  const scrollPosition = window.scrollY;
+  const documentHeight =
+    document.documentElement.scrollHeight - window.innerHeight;
+  const scrolled = (scrollPosition / documentHeight) * 100;
+
+  scrollIndicator.style.width = scrolled + "%";
 });
