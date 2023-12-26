d71a243c9765af591687c6522e76bf91ffc8567d
Joy_Boy <mihirkhetalpar111@gmail.com>
2023-12-26 16:56:08 +0530

    Category Pages file Add

---
+++ BooksHub/JS/Common.js
@@ -0,0 +1,7 @@
+                                                    // PRELOADER 
+document.addEventListener("DOMContentLoaded", function() {
+let loader = document.getElementById("preloader");
+setTimeout(function() {
+loader.style.display = "none";
+   }, 2000); 
+});
