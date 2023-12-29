52db5fb58c3168e12dd9ad7cd2fc7a2b4cc32292
Joy_Boy <mihirkhetalpar111@gmail.com>
2023-12-29 20:48:12 +0530

    Home page is now responsive for 400px width mobile

--- BooksHub/JS/Books_Hub.js
+++ BooksHub/JS/Books_Hub.js
@@ -1,87 +1,72 @@
-                                                    // PRELOADER 
-                                                    
-document.addEventListener("DOMContentLoaded", function() {
-let loader = document.getElementById("preloader");
-setTimeout(function() {
-loader.style.display = "none";
-   }, 2000); 
+// PRELOADER
+
+document.addEventListener("DOMContentLoaded", function () {
+  let loader = document.getElementById("preloader");
+  setTimeout(function () {
+    loader.style.display = "none";
+  }, 2000);
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  const menuToggle = document.querySelector('.menu-toggle');
-  const navLinks = document.querySelector('.nav-links');
+// NAVBAR
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
 
-  menuToggle.addEventListener('click', function() {
-    navLinks.classList.toggle('show');
+  menuToggle.addEventListener("click", function () {
+    navLinks.classList.toggle("show");
   });
 
   // Close the nav links if clicked outside the navbar area
-  document.addEventListener('click', function(e) {
+  document.addEventListener("click", function (e) {
     if (!menuToggle.contains(e.target) && !navLinks.contains(e.target)) {
-      navLinks.classList.remove('show');
+      navLinks.classList.remove("show");
     }
   });
 
   // Hide the navigation links if window width is more than 400px on load
   function handleNavDisplay() {
     if (window.innerWidth > 400) {
-      navLinks.classList.remove('show');
+      navLinks.classList.remove("show");
     }
   }
 
   // Listen for window resize and update navigation links display accordingly
-  window.addEventListener('resize', handleNavDisplay);
+  window.addEventListener("resize", handleNavDisplay);
 
   // Initially check the window width to show/hide navigation links
   handleNavDisplay();
 });
 
-
-                                // Detect scroll events and update the width of the scroll indicator
-window.addEventListener('scroll', function() {
-  const scrollIndicator = document.querySelector('.scroll-indicator');
+// Detect scroll events and update the width of the scroll indicator
+window.addEventListener("scroll", function () {
+  const scrollIndicator = document.querySelector(".scroll-indicator");
   const scrollPosition = window.scrollY;
-  const documentHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const documentHeight =
+    document.documentElement.scrollHeight - window.innerHeight;
   const scrolled = (scrollPosition / documentHeight) * 100;
 
-  scrollIndicator.style.width = scrolled + '%';
+  scrollIndicator.style.width = scrolled + "%";
 });
 
-                                                                                                                                           
-                                                    // Banner Image Slider
-const slides = document.querySelectorAll(".slide");
-const slideWidth = slides[0].clientWidth;
-const radioButtons = document.querySelectorAll(".slider-nav label");
-let currentIndex = 0;
+// Banner Image Slider
+let slideIndex = 0;
+showSlides();
 
-function showSlide(index) {
-  const offset = -index * slideWidth;
-  slides.forEach((slide) => {
-    slide.style.transform = `translateX(${offset}px)`;
-  });
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
-
-radioButtons.forEach((radio, index) => {
-  radio.addEventListener("click", () => {
-    currentIndex = index;
-    showSlide(currentIndex);
-  });
-});
-
-// Initialize the slider to show the first slide
-showSlide(currentIndex);
-
-// Auto slide every 5 seconds
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % slides.length;
-  showSlide(currentIndex);
-  radioButtons[currentIndex].checked = true;
-}
-
-setInterval(nextSlide, 4000);
-
-
-
-                                                                                                     
-
-
